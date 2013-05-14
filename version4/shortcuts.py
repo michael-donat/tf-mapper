6395c307ee70bca5af2fb4310c65e2b7fd5a6441
Michael Donat <thornag@gmail.com>
2013-05-14 09:31:26 +0200

    Update shortcuts.py

--- version4/shortcuts.py
+++ version4/shortcuts.py
@@ -1,5 +1,10 @@
 def shortcuts():
     return [
         dict(name='Novigrad',           room='dd6feece-8c43-11e2-8687-9c4e368dac0c'),
-        dict(name='Pustynia',           room='77aee47c-8983-11e2-8c92-9c4e368dac0c')
+        dict(name='Pustynia',           room='77aee47c-8983-11e2-8c92-9c4e368dac0c'),
+        dict(name='Bodrog',             room='e17b03b4-8923-11e2-9217-9c4e368dac0c'),
+        dict(name='Scala',             room='4002f2de-8b7c-11e2-af6b-9c4e368dac0c'),
+        dict(name='Arond',              room='51b5c2cc-8cb7-11e2-a793-9c4e368dac0c'),
+        dict(name='Blaviken',           room='27f4367e-89df-11e2-9919-9c4e368dac0c'),
+        dict(name='Deavon',             room='b658625a-89b2-11e2-878b-2af7728612b4'),
     ]
