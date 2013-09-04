30d15c1575e515896334f59cc07dcfbb7cc29163
Michal Donat <thornag@gmail.com>
2013-09-04 18:37:25 +0100

    final tiker for undo

--- model/model.py
+++ model/model.py
@@ -662,6 +662,14 @@ class Navigator(QtCore.QObject):
 
     def undoCreation(self):
 
+        if self._lastCreatedRoom is not None or self._lastCreatedLink is not None:
+            if self.__registry.previouslyVisitedRoom is not None:
+                roomId = self.__registry.previouslyVisitedRoom.getId()
+                roomId = str(roomId)
+                if roomId not in self.__map.rooms().keys(): return False
+                room = self.__map.rooms()[roomId]
+                self.markVisitedRoom(room)
+
         if self._lastCreatedRoom is not None:
             self._lastCreatedRoom.delete()
             self._lastCreatedRoom = None
